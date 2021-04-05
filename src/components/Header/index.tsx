d756cbd3850abe185711f6b7b4a62f774a0d1c34
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-04-05 13:36:15 -0300

    feat: responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from './Logo';
 import { Search } from './Search';
 import { Profile } from './Profile';
 import { NotificationNav } from './NotificationNav';
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,6 +25,16 @@ export function Header() {
       px='6'
       align='center'
     >
+      { !isWideVersion && (
+        <IconButton 
+        onClick={onOpen} 
+        icon={<Icon as={RiMenuLine} />} 
+        fontSize='24' 
+        variant='unstyled' 
+        aria-label='Open navigation' 
+        mr='2' />
+      )}
+
       <Logo />
 
       { isWideVersion && <Search /> }
